5d05f057ce4a1e1482f3f4ea6a361c9aaea7fea2
Alper Gokcehan <mail@alperg.com>
2015-09-18 08:01:24 -0400

    jshint fix

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 /*jslint node: true */
+/*jshint unused: false */
 'use strict';
 
 /**
@@ -13,7 +14,7 @@ if (process.env.NEW_RELIC_LICENSE_KEY && process.env.NEW_RELIC_LICENSE_KEY !== '
 
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -45,7 +46,7 @@ var config = {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res) {
+  app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -56,7 +57,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res) {
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
@@ -85,7 +86,7 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res) {
+app.use(function(req, res, next) {
   var err = new Error('404 - Not Found!');
   err.status = 404;
   res.render('404', {
